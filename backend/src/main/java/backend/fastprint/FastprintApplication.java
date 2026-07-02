95cd4cac564cbdf9364c7b06d7758a5c58f2ee6f
Leroy-Zeeboy <djaoweleroy88@gmail.com>
2026-07-02 06:53:54 +0100

    feat: projet Spring Boot FastPrint

---
+++ backend/src/main/java/backend/fastprint/FastprintApplication.java
@@ -0,0 +1,13 @@
+package backend.fastprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastprintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastprintApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/backend/fastprint/FastprintApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.fastprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastprintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
